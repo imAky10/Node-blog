da656d2dcbe5fd78756c06410ffa2bce21942874
Amit <amit.aky10@gmail.com>
2020-06-19 23:45:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+require('dotenv').config()
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+const port = 5000;
+
+app.listen(port, ()=> 
+  console.log(`Server running at port ${port}`))
